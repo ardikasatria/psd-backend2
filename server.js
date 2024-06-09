54efa1580dd94c471ffaa8430a59ffa0987155e6
projeksainsdata <projeksainsdata@gmail.com>
2024-06-10 02:25:49 +0700

    empat

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ import Comment from "./Schema/Comment.js";
 import ToDo from './Schema/ToDo.js';
 import Notepad from './Schema/Notepad.js';
 import SavedBlog from './Schema/SavedBlog.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const server = express();
 let PORT = 3000;
